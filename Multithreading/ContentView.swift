aa50de804c4ac77b5d755abbc4ce39b8adebbbee
Christian Skorobogatow <christiansko@icloud.com>
2022-07-13 19:27:16 +0200

    feat(): multithread examples

--- Multithreading/ContentView.swift
+++ Multithreading/ContentView.swift
@@ -7,10 +7,62 @@
 
 import SwiftUI
 
+
+class BackgroundThreadViewModel: ObservableObject {
+    
+    @Published var dataArray = [String]()
+    
+    func fetchData() {
+        
+        DispatchQueue.global(qos: .background).async {
+            let newData = self.downloadedData()
+            
+            print("CHECK 1 \(Thread.isMainThread)")
+            print("CHECK 1 \(Thread.current)")
+            
+            DispatchQueue.main.async {
+                self.dataArray = newData
+                
+                print("CHECK 2 \(Thread.isMainThread)")
+                print("CHECK 2 \(Thread.current)")
+            }
+        }
+    }
+    
+    func downloadedData() -> [String] {
+        var data: [String] = []
+        
+        for x in 0..<100 {
+            data.append("\(x)")
+            print(data)
+        }
+        
+        return data
+    }
+    
+}
+
 struct ContentView: View {
+    
+    @StateObject var vm = BackgroundThreadViewModel()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ScrollView {
+            LazyVStack(spacing: 20) {
+                Text("LOAD DATA")
+                    .font(.largeTitle)
+                    .fontWeight(.semibold)
+                    .onTapGesture {
+                        vm.fetchData()
+                    }
+                
+                ForEach(vm.dataArray, id: \.self) { item in
+                    Text(item)
+                        .font(.headline)
+                        .foregroundColor(.red)
+                }
+            }
+        }
     }
 }
 
