fdb36d6eb9bb2904fdc900e793e4677e0a813e80
Christian Skorobogatow <christiansko@icloud.com>
2022-07-13 18:53:02 +0200

    Initial Commit

---
+++ Multithreading/MultithreadingApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultithreadingApp.swift
+//  Multithreading
+//
+//  Created by Christian Skorobogatow on 13/7/22.
+//
+
+import SwiftUI
+
+@main
+struct MultithreadingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
